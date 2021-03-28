fd165aa40c3abbb3c5864401ca6a4a7535253fff
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-03-28 21:05:23 +0200

    Refactor delete import

--- python/tests/test_transformer.py
+++ python/tests/test_transformer.py
@@ -3,15 +3,12 @@ import pickle
 import sys
 import unittest
 
-import pandas as pd
-
 sys.path.append(f"{pathlib.Path().absolute()}/etl")
 
 from etl.transformer import Transformer
 
 
 class TestTransformer(unittest.TestCase):
-
     def setUp(self):
         with open("tests/files/pickle/dfs_from_db", "rb") as f:
             self.dfs = pickle.load(f)
@@ -23,4 +20,3 @@ class TestTransformer(unittest.TestCase):
             subset=primary_keys
         )
         self.assertEqual(len(result_df), len(result_df_without_duplicated_primary_keys))
-
